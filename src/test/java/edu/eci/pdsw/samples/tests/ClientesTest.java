da382269b5b5499ad4d8293edb35aaf4c2c15549
gabrielp890 <2123366@linux15.labinfo.is.escuelaing.edu.co>
2017-03-02 13:02:04 -0500

    prueba de agregar cliente

--- src/test/java/edu/eci/pdsw/samples/tests/ClientesTest.java
+++ src/test/java/edu/eci/pdsw/samples/tests/ClientesTest.java
@@ -5,13 +5,20 @@
  */
 package edu.eci.pdsw.samples.tests;
 
+import edu.eci.pdsw.samples.entities.Cliente;
 import edu.eci.pdsw.samples.services.ExcepcionServiciosAlquiler;
+import edu.eci.pdsw.samples.services.ServiciosAlquiler;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
+ *Intento de agregar cliente con el atributo vetado =true
+ * addItem1: cuando el atributo vetado es igual a true, entonces no se debe permitir agregar a un cliente
+ * 
+ * El cliente ya se encuentra registrado
+ * clienteRegitrado: se invoca el metodo consultarCliente y si ya se encuentra, entonces no se debe agregar 
+ * 
  * 
  */
 public class ClientesTest {
@@ -28,6 +35,18 @@ public class ClientesTest {
     public void additems1() throws ExcepcionServiciosAlquiler{
     	
     }
+    
+    public void addCliente() throws ExcepcionServiciosAlquiler{
+        ServiciosAlquiler sa=ServiciosAlquiler.getInstance();
+        
+        Cliente cl= new Cliente("Juan Perez",3842,"24234","calle 123","aa@gmail.com");
+        
+        if(sa.consultarCliente(cl.getDocumento())!=null){
+            assertEquals("el cliente ya se encuentra registrado", null,sa.consultarCliente(cl.getDocumento()));
+        }
+    }
+    
+      
     
     
     
